dddc8f3c5bd32154bb0ef388ca681d5cf0866c99
en1ma <aboudraa@cloudbees.com>
2022-09-02 22:14:15 +0200

    Update stretched-string.cpp
    Hello I'm a signed commit

--- C++/stretched-string.cpp
+++ C++/stretched-string.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::string access_level = "user";
-    if (access_level.compare("user‮ ⁦// Check if admin⁩ ⁦")) {
-        std::cout << "You are an admin.\n";
-    }
-    return 0;
-}
+Hello I'm a signed commit
